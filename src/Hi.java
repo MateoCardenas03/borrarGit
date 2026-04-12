b6ceee6a47defeba51334032b6fe9e9ee8d3e528
Mateo <mateo.cardenas03@epn.edu.ec>
2026-04-12 10:24:17 -0500

    Archivos de main

--- src/Hi.java
+++ src/Hi.java
@@ -2,7 +2,7 @@ package src;
 public class Hi {
 public static void main(String[] args) {
     System.out.println("Hi, amo!");
-    System.out.println( "Hi, cardenas!");
-    System.out.println("Todo bien desde la nuve, cardemas");
+    System.out.println( "Hi, Mateo Cardenas !");
+    System.out.println("Todo bien desde la nuve, Cardenas");
 }
 }
